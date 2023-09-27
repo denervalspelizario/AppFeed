ea0b67164df4219dd05426cf9c3f7ea0a28e5d00
denervalspelizario <denervalspelizario@gmail.com>
2023-09-27 11:08:16 -0300

    Estruturando base do app e css global

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from './Post'
+import { Header } from './components/Header'
+import './global.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Post 
+        author="Dener S P" 
+        content = "lorem ipsum uHlk~   KMmHGN"
+      />
+    </>
+  )
+}
+
+export default App
